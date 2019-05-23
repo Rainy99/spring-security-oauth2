c9577281e99595942fc97251cf3f849173ba5ca8
devzhou <zhouyu@devzhou.cn>
2019-05-23 21:30:04 +0800

    Add jdbc TokenStore

--- src/main/java/cn/devzhou/sso/config/AuthorizationServerConfig.java
+++ src/main/java/cn/devzhou/sso/config/AuthorizationServerConfig.java
@@ -1,11 +1,19 @@
 package cn.devzhou.sso.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * @author devzhou
@@ -13,6 +21,18 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private DataSource dataSource;
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
         oauthServer.passwordEncoder(new PasswordEncoder() {
@@ -31,20 +51,25 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("api1").secret("123")
-                .redirectUris("http://localhost:9001/callback")
-                .authorizedGrantTypes("authorization_code","refresh_token")
-                .scopes("read_userinfo", "read_contacts")
-                .resourceIds("oauth2-resource")
-                .accessTokenValiditySeconds(1200)
-                .refreshTokenValiditySeconds(50000)
-                .and()
-                .withClient("clientapp").secret("112233")
-                .redirectUris("http://localhost:9001/callback")
-                .authorizedGrantTypes("implicit")
-                .scopes("read_userinfo", "read_contacts");
+        clients.jdbc(dataSource);
+//                .withClient("api1").secret("123")
+//                .redirectUris("http://localhost:9001/callback")
+//                .authorizedGrantTypes("authorization_code","refresh_token")
+//                .scopes("read_userinfo", "read_contacts")
+//                .resourceIds("oauth2-resource")
+//                .accessTokenValiditySeconds(1200)
+//                .refreshTokenValiditySeconds(50000)
+//                .and()
+//                .withClient("clientapp").secret("112233")
+//                .redirectUris("http://localhost:9001/callback")
+//                .authorizedGrantTypes("implicit")
+//                .scopes("read_userinfo", "read_contacts");
 
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore());
     }
 
 }
--- src/main/java/cn/devzhou/sso/config/SecurityConfiguration.java
+++ src/main/java/cn/devzhou/sso/config/SecurityConfiguration.java
@@ -1,6 +1,9 @@
 package cn.devzhou.sso.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,5 +35,11 @@ public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .passwordEncoder(passwordEncoder)
                 .withUser("admin").password(passwordEncoder.encode("123456")).roles("ADMIN");
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
